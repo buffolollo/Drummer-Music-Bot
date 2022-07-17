5ab128dd1632082fbbc1ed6f9724ad63588d1890
buffolollo <elia.stef006@gmail.com>
2022-07-17 23:28:02 +0200

    Added mc server command

---
+++ commands/util/minecraft.js
@@ -0,0 +1,44 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+const MC = require("minecraft-server-util");
+
+module.exports = {
+  name: "mcstats",
+  aliases: ["mc", "minecrfat"],
+  d: "Get stats of a minecraft server.",
+  /**
+   *
+   * @param {Message} message
+   * @param {Client} client
+   */
+  async execute(client, message, args) {
+    const IP = "185.229.237.130";
+    const Port = "25565";
+
+    const Embed = new MessageEmbed()
+      .setColor("#1ad4a8")
+      .setTitle("Devvolt Network | Stats");
+
+    MC.status(IP, parseInt(Port))
+      .then((response) => {
+        Embed.setDescription(
+          `> Server IP: \`devvolt.dev\`\n> Server Port: \`${parseInt(
+            Port
+          )}\`\n> Server Motd: \`${response.motd.clean}\`\n> Server Players: \`${
+            response.players.online
+          }\`\n> Server Max Players\`${
+            response.players.max
+          }\`\n> Server Version: \`${response.version.name}\``
+        );
+        message.channel.send({ embeds: [Embed] });
+      })
+      .catch((err) => {
+        message.channel.send({
+          embeds: [
+            Embed.setColor("RED").setDescription(
+              `> There was an error, check the server IP and Port.\n> \n> Error: ${err}`
+            ),
+          ],
+        });
+      });
+  },
+};
