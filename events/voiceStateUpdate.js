3377850e03c999b0e0744f9d53bdc513ae019ddf
buffolollo <elia.stef006@gmail.com>
2022-07-26 10:49:39 +0200

    Updated code

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,20 +1,20 @@
-const { VoiceState, Client } = require("discord.js")
+const { VoiceState, Client } = require("discord.js");
 
 module.exports = {
-    name: "voiceStateUpdate",
-    /**
-     * 
-     * @param {VoiceState} oldstate 
-     * @param {VoiceState} newstate 
-     * @param {Client} client 
-     */
-    execute(oldstate, newstate, client){
-        if(oldstate.member.id != "928001727309946932") return;
-        const queue = oldstate.client.queue.get(oldstate.guild.id)
-        const deletequeue = (id) => oldstate.client.queue.delete(id);
-        if(oldstate.channelId && !newstate.channel){
-            if(queue.player) queue.player.stop();
-            if (queue) deletequeue(oldstate.guild.id);
-        }
+  name: "voiceStateUpdate",
+  /**
+   *
+   * @param {VoiceState} oldstate
+   * @param {VoiceState} newstate
+   * @param {Client} client
+   */
+  execute(oldstate, newstate, client) {
+    if (oldstate.member.id != "928001727309946932") return;
+    const queue = oldstate.client.queue.get(oldstate.guild.id);
+    const deletequeue = (id) => oldstate.client.queue.delete(id);
+    if (oldstate.channelId && !newstate.channel) {
+      if (queue.player) queue.player.stop();
+      if (queue) deletequeue(oldstate.guild.id);
     }
-}
+  },
+};
