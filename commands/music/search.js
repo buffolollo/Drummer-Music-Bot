e0062597a80e659595a6dfff03bd5290f737a9d9
buffolollo <elia.stef006@gmail.com>
2022-07-28 23:30:17 +0200

    Fixed bug

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,6 +1,7 @@
 const { Client, Message, EmbedBuilder, Collection } = require("discord.js");
 const ytdl = require("discord-ytdl-core");
 const ytsr = require("yt-search");
+const searcher = require("youtube-sr").default;
 const yt = require("ytdl-core");
 const send = require("../../utils/src/send");
 const alreadyCollector = new Collection();
@@ -27,42 +28,43 @@ module.exports = {
     const deletequeue = (id) => message.client.queue.delete(id);
 
     if (!args.length) return error("**You didn't give me a song to play!**");
-    ytsr(args.join(" "), (err, res) => {
-      if (err) return error("Sorry, something went wrong!");
-      let videos = res.videos.slice(0, 10);
-      let resp = "";
-      for (var i in videos) {
-        resp += `**[${parseInt(i) + 1}]:** \`${videos[i].title}\`\n`;
-      }
-      resp += `\nChoose a number from \`1-${videos.length}\``;
-      send(message, resp);
-      const filter = (m) =>
-        !isNaN(m.content) &&
-        m.content < videos.length + 1 &&
-        m.content > 0 &&
-        m.author.id == message.author.id;
-      const collectorString = `${message.author.id}${message.channel.id}`;
-      const currentCollector = alreadyCollector.get(collectorString);
-      if (currentCollector) {
-        currentCollector.stop();
-      }
-      const collector = message.channel.createMessageCollector({
-        filter,
-        time: 30000,
-        errors: ["time"],
-      });
-      alreadyCollector.set(collectorString, collector);
-      collector.video = videos;
-      collector.on("collect", (m) => {
-        let File = require("./play");
-        File.execute(client, message, [videos[parseInt(m.content) - 1].url]);
-        collector.stop();
-      });
-      collector.on("end", (collected, reason) => {
-        if (reason == "time") {
-          error("The time is up, if you want you can redo the command");
+
+    searcher
+      .search(args.join(" "), { limit: 10, type: "video" })
+      .then((result) => {
+        let resp = "";
+        for (var i = 0; i < result.length; i++) {
+          resp += `**[${parseInt(i) + 1}]** \`${result[i].title}\`\n`;
         }
+        resp += `\nChoose a number from \`1-${result.length}\``;
+        send(message, resp);
+        const filter = (m) =>
+          !isNaN(m.content) &&
+          m.content < result.length + 1 &&
+          m.content > 0 &&
+          m.author.id == message.author.id;
+        const collectorString = `${message.author.id}${message.channel.id}`;
+        const currentCollector = alreadyCollector.get(collectorString);
+        if (currentCollector) {
+          currentCollector.stop();
+        }
+        const collector = message.channel.createMessageCollector({
+          filter,
+          time: 30000,
+          errors: ["time"],
+        });
+        alreadyCollector.set(collectorString, collector);
+        collector.video = result;
+        collector.on("collect", (m) => {
+          let File = require("../playBased/play");
+          File.execute(client, message, [result[parseInt(m.content) - 1].url]);
+          collector.stop();
+        });
+        collector.on("end", (collected, reason) => {
+          if (reason == "time") {
+            error("The time is up, if you want you can redo the command");
+          }
+        });
       });
-    });
   },
 };
