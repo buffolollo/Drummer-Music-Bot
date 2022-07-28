e5a121bb7ab646665f8d8b1b17356a20b6b45487
buffolollo <elia.stef006@gmail.com>
2022-07-28 13:40:00 +0200

    Minor changes

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -10,9 +10,9 @@ module.exports = {
    */
   execute(oldstate, newstate, client) {
     if (oldstate.member.id != "928001727309946932") return;
-    const queue = oldstate.client.queue.get(oldstate.guild.id);
     const deletequeue = (id) => oldstate.client.queue.delete(id);
     if (oldstate.channelId && !newstate.channel) {
+      const queue = oldstate.client.queue.get(oldstate.guild.id);
       if (queue.player) queue.player.stop();
       if (queue) deletequeue(oldstate.guild.id);
     }
