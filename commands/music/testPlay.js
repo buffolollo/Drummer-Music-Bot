9bd9984b7e2b8ca1c7c8827b8956fe8bdedc479e
buffolollo <elia.stef006@gmail.com>
2022-07-29 23:24:27 +0200

    Nothing

--- commands/music/testPlay.js
+++ commands/music/testPlay.js
@@ -13,6 +13,7 @@ const ytsr = require("youtube-sr").default;
 // const { getData, getPreview, getTracks, getDetails } = require("spotify-url-info")(fetch);
 const fetch = require("isomorphic-unfetch");
 const spotify = require("spotify-url-info")(fetch);
+const { join } = require("node:path");
 
 const yt = require("ytdl-core");
 let ytdl = require("discord-ytdl-core");
@@ -20,7 +21,7 @@ let ytdl = require("discord-ytdl-core");
 module.exports = {
   name: "testplay",
   aliases: ["tp"],
-  // voice: true,
+  //voice: true,
   staff: true,
   d: "Riproduci una canzone!",
   /**
@@ -33,11 +34,29 @@ module.exports = {
   async execute(client, message, args) {
     let channel = message.member.voice.channel;
     let deletequeue = (id) => message.client.queue.delete(id);
-
-    // const link = "https://www.youtube.com/watch?v=X-5ACIGzSw0"
-
-    // const data = await ytsr.search(link, { type: "video", limit: 1 })
-
-    // console.log(data[0]);
+    const player = createAudioPlayer();
+    const resource = createAudioResource(
+      "https://cdn.discordapp.com/attachments/994708980314144839/1002683248343785542/pati.mp3",
+      { inlineVolume: true }
+    );
+    player.play(resource);
+    if (args[0] == "1") {
+      const connection1 = await joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+      resource.volume.setVolumeLogarithmic(100 / 100);
+      connection1.subscribe(player);
+    }
+    if (args[0] == "2") {
+      const connection2 = await joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+      resource.volume.setVolumeLogarithmic(100 / 100);
+      connection2.subscribe(player);
+    }
   },
 };
